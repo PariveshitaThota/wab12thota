96dfeb5acc430476f75a8edc43f4176a0dd70213
PariveshitaThota <77807971+PariveshitaThota@users.noreply.github.com>
2021-03-31 19:24:53 -0500

    committed

--- routes/stars.js
+++ routes/stars.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('stars', {title: "Stars", query: query });
+  let query=req.query
+    console.log(`rows ${query.rows}`)
+    console.log(`cols ${query.cols}`)
+    res.render('stars', {title: "Stars", query: query });
 });
 
 module.exports = router;
